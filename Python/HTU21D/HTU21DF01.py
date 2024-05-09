901bc3613868525392041c2116df852b9bff0624
Bruce Tsao <prgbruce@gmail.com>
2024-05-09 10:14:54 +0800

    commit all

--- Python/HTU21D/HTU21DF01.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin, I2C, SoftI2C
-from micropython_htu21df import htu21df
-i2c = SoftI2C(scl=Pin(22),sda=Pin(21),freq=100_000)
-#i2c = I2C(scl=Pin(22),sda=Pin(21),freq=100_000)
-htu21df = htu21df.HTU21DF(i2c)
-temp = htu21df.temperature
-rh = htu21df.humidity
-print("Temperature:",temp)
-print("Humity:",rh)
--- Python/HTU21D/HTU21DV02.py
+++
@@ -1,9 +0,0 @@
-from HTU21D	import HTU21D#使用HTU21DF
-from myLib	import * #使用使用者自訂函數
-from machine import Pin, I2C, SoftI2C
-i2c = SoftI2C(scl=Pin(22),sda=Pin(21),freq=100_000)
-lectura = HTU21D(22,21)
-hum = lectura.humidity
-temp = lectura.temperature
-print('Humedad: ', + hum)
-print('Temperatura: ', + temp)
